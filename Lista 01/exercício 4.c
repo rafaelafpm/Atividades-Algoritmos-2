afaf242416b3e0fa3231d9d1eb5c14cef8f373cb
Rafaela França <rafaela.mariano@ufu.br>
2025-07-30 16:03:22 -0300

    Create exercício 4.c
    Faça um algoritmo utilizando os comando for, while e do..while que mostra uma contagem 
    regressiva na tela, iniciando em 10 e terminando em 0. Mostrar uma mensagem “FIM!” após 
    a contagem.

---
+++ Lista 01/exercício 4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int i ;
+printf ("Digite um numero para ser feito seu fatorial:");
+scanf ("%d",&i);
+for (i=i;i>0;i--){
+printf("%d\n",i);
+}
+return 0;
+}
+
+
+int i;
+scanf ("%d",&i);
+while (i>0)
+{
+printf ("%d\n",i);
+i--;
+}
+return 0;
+}
+
+
+
+int i;
+scanf ("%d",&i);
+do {
+printf ("%d\n",i);
+i--;
+}
+while (i>0);
+}
