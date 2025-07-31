14735c6321f3238e52497abeef737b07a81748a1
Rafaela França <rafaela.mariano@ufu.br>
2025-07-31 16:26:48 -0300

    Create exercício 21.c
    Crie uma função que receba como parâmetro um valor inteiro e gere como saída n 
    linhas com pontos de exclamação, conforme o exemplo abaixo (para n = 5):
    !
     !!
     !!!
     !!!!
     !!!!!

---
+++ Lista 02/exercício 21.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+int funcao(int b){
+    int i;
+    for (i=0;i<b;i++){
+for(i=0;i<b;i++){
+printf("!",i);
+}
+printf("\n");
+return 1;
+}
+}
+int main (){
+int x;
+printf(" Digite um numero para gerar '!':");
+scanf("%d",&x);
+funcao(x);
+return 0;
+}
